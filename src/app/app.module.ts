3f1580d55d3ed66919a08afe38b46778a0838dc2
cevau <christoph_voigt@yahoo.de>
2018-12-20 15:43:54 +0000

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GeolocComponent } from './geoloc/geoloc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeolocComponent
   ],
   imports: [
     BrowserModule
--- src/app/geoloc/geoloc.component.ts
+++ src/app/geoloc/geoloc.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '../location';
 
 @Component({
   selector: 'app-geoloc',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./geoloc.component.css']
 })
 export class GeolocComponent implements OnInit {
+  location : Location = {
+    longitude: 12.4,
+    latitude: 66.4
+  };
 
+  // longitude = '7.0';
+  // latitude = '50.0';
   constructor() { }
 
   ngOnInit() {
